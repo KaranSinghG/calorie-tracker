0f512a5536b899277cb0e478e9539e3a0d619d6f
KaranSinghG <karansinghg@ymail.com>
2026-06-10 21:39:57 +0530

    feat: add AppNavigation component for screen navigation

---
+++ frontend/src/navigation/AppNavigation.tsx
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import RegisterScreen from "../screens/RegisterScreen";
+import HomeScreen from "../screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "../screens/LoginScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
